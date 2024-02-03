37cf21c52c1e85064b76bf71b5855edde0823a2b
sara latorre <saralatorre97@gmail.com>
2024-02-03 22:33:11 +0100

    added fonts

---
+++ src/app/ui/fonts.ts
@@ -0,0 +1,13 @@
+import { Yeseva_One, DM_Sans } from 'next/font/google';
+
+export const yeseva = Yeseva_One({
+    subsets: ['latin'],
+    display: 'swap',
+    weight: '400'
+});
+
+export const dm_sans = DM_Sans({
+    subsets: ['latin'],
+    display: 'swap',
+    weight: '400'
+});
